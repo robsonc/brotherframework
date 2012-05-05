343f2283d83c2b1b0d7ea8a447825793f924336a
Robson Coutinho <robson_coutinho@yahoo.com.br>
2012-05-04 23:56:38 -0400

    criacao da pasta para documentacao do framework

---
+++ public/docs/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Documentação Brother Framework</title>
+
+		<style type="text/css">
+			body {font-family: Georgia;}
+		</style>
+	</head>
+	<body>
+		<h1>Documentação Brother Framework</h1>
+	</body>
+</html>
